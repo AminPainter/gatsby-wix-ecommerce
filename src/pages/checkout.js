29c1da7f63fa3b4e274556e17cb647074c6817a3
AminPainter <aminpainter786@gmail.com>
2023-11-13 12:30:37 +0530

    arrow func return

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -3,15 +3,13 @@ import React from 'react';
 import { Grid, Section } from 'ui';
 import { CheckoutForm, CheckoutOrderSummary } from 'components/checkout-page';
 
-const CheckoutPage = () => {
-  return (
-    <Section>
-      <Grid columns={[1.5, 1]} gap={10}>
-        <CheckoutForm />
-        <CheckoutOrderSummary />
-      </Grid>
-    </Section>
-  );
-};
+const CheckoutPage = () => (
+  <Section>
+    <Grid columns={[1.5, 1]} gap={10}>
+      <CheckoutForm />
+      <CheckoutOrderSummary />
+    </Grid>
+  </Section>
+);
 
 export default CheckoutPage;
