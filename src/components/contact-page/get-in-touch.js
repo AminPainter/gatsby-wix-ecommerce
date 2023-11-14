5c92194d2eddf60c90e89bd16164faf38dcea659
AminPainter <aminpainter786@gmail.com>
2023-11-14 15:51:34 +0530

    formik on contact form

--- src/components/contact-page/get-in-touch.js
+++ src/components/contact-page/get-in-touch.js
@@ -1,37 +1,90 @@
-import { Box, Container, Stack, TextField, Typography, styled } from '@mui/material';
 import React from 'react';
+import { Box, Container, Stack, TextField, Typography, styled } from '@mui/material';
+import { Formik } from 'formik';
 
 import { Button, Grid, Heading, Section } from 'ui';
 
-const GetInTouch = () => (
-  <Section chip='Contact' title='contact us' maxWidth='xl'>
-    <Grid columns={[1.5, 1]} gap={0}>
-      <ColoredBox>
-        <Container maxWidth='sm' sx={{ height: '100%' }}>
-          <Stack justifyContent='center' gap={1} height='100%'>
-            <Heading variant='tertiary'>get in touch</Heading>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id leo tempor, congue
-              justo at, lobortis orci.
-            </Typography>
-          </Stack>
-        </Container>
-      </ColoredBox>
+const initialValues = {
+  firstName: '',
+  lastName: '',
+  emailAddress: '',
+  message: '',
+};
 
-      <Box p={10}>
-        <ContactForm>
-          <Grid gap={2} mb={2}>
-            <TextField label='First name' />
-            <TextField label='Last name' />
-            <TextField label='Email address' sx={{ gridColumn: '1 / -1' }} />
-            <TextField label='Message' sx={{ gridColumn: '1 / -1' }} multiline minRows={4} />
-          </Grid>
-          <Button>Send</Button>
-        </ContactForm>
-      </Box>
-    </Grid>
-  </Section>
-);
+const GetInTouch = () => {
+  const handleSubmit = async values => {
+    console.log(values);
+  };
+
+  return (
+    <Section chip='Contact' title='contact us' maxWidth='xl'>
+      <Grid columns={[1.5, 1]} gap={0}>
+        <ColoredBox>
+          <Container maxWidth='sm' sx={{ height: '100%' }}>
+            <Stack justifyContent='center' gap={1} height='100%'>
+              <Heading variant='tertiary'>get in touch</Heading>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id leo tempor, congue
+                justo at, lobortis orci.
+              </Typography>
+            </Stack>
+          </Container>
+        </ColoredBox>
+
+        <Box p={10}>
+          <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+            {formik => (
+              <ContactForm onSubmit={formik.handleSubmit}>
+                <Grid gap={2} mb={2}>
+                  <TextField
+                    label='First name'
+                    name='firstName'
+                    value={formik.values.firstName}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    required
+                  />
+
+                  <TextField
+                    label='Last name'
+                    name='lastName'
+                    value={formik.values.lastName}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    required
+                  />
+
+                  <TextField
+                    label='Email address'
+                    type='email'
+                    name='emailAddress'
+                    value={formik.values.emailAddress}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    sx={{ gridColumn: '1 / -1' }}
+                    required
+                  />
+
+                  <TextField
+                    label='Message'
+                    name='message'
+                    value={formik.values.message}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    sx={{ gridColumn: '1 / -1' }}
+                    multiline
+                    minRows={4}
+                  />
+                </Grid>
+                <Button type='submit'>Send</Button>
+              </ContactForm>
+            )}
+          </Formik>
+        </Box>
+      </Grid>
+    </Section>
+  );
+};
 
 export default GetInTouch;
 
